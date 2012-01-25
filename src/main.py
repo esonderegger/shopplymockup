76c835975796e372a81d4910a69c053642ba31c9
Evan Sonderegger <evan.sonderegger@gmail.com>
2012-01-25 18:46:04 -0500

    worked on main header

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 
+def mainHeader():
+  print '<header>'
+  print '<div id="logo">'
+  print '<img src="/img/ShopplyLogo.png" alt="Shopply Logo" />'
+  print '</div>'
+  print '<div id="headerLinks">'
+  print 'Home | My Deals | Search'
+  print '</div>'
+  print '<div id="welcomeUser">'
+  print 'Welcome Dave'
+  print '</div>'
+  print '</header>'
+
 print 'Content-Type: text/html \n'
 print '<!DOCTYPE html>'
 print '<html lang="en">'
+print '<head>'
+print '<meta charset="utf-8" />'
+print '<title>Shopply</title>'
+print '<link href="/css/shopply.css" rel="stylesheet" />'
+print '</head>'
 print '<body>'
-print '<h1>Shopply</h1>'
+mainHeader()
 print '</body>'
 print '</html>'
