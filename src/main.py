fc0baad36026fb13f1a80041a82133f9a25bf03d
Evan Sonderegger <evan.sonderegger@gmail.com>
2012-01-25 18:19:24 -0500

    basic skeleton

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+print 'Content-Type: text/html \n'
+print '<!DOCTYPE html>'
+print '<html lang="en">'
+print '<body>'
+print '<h1>Shopply</h1>'
+print '</body>'
+print '</html>'
