3fba9927a6e1dc60668e4b5824c65b627838e71b
Evan Sonderegger <evan.sonderegger@gmail.com>
2012-01-26 19:15:48 -0500

    basic coverflow functionality

--- src/browse.py
+++ src/browse.py
@@ -8,7 +8,9 @@ objectCount = 0
 for line in sampleFile:
   lineList = line.split(';')
   print '<div class="coverflow" id="object' + str(objectCount) + '">'
-  print '<h2>' + lineList[0] + ' - ' + lineList[1] + ' - ' + lineList[2] + '</h2>'
+  print '<div>' + lineList[0] + '</div>'
+  print '<div>' + lineList[1] + '</div>'
+  print '<div>' + lineList[2] + '</div>'
   print '</div>'
   objectCount += 1
 
--- src/js/shopply.js
+++ src/js/shopply.js
@@ -15,4 +15,42 @@ function synchronousGetFile(url) {
 function initMainPage() {
   var browseData = synchronousGetFile("/browse");
   document.getElementById("dealsContent").innerHTML = browseData;
+  moveCenter(3);
+}
+function moveCenter(num){
+  var llBlock = getBlockByNum(num - 2);
+  llBlock.style.webkitTransform = "rotateY(80deg) scale(0.5)";
+  llBlock.style.left = "200px";
+  llBlock.style.opacity = 0.3;
+  llBlock.style.zIndex = 1;
+  var lBlock = getBlockByNum(num - 1);
+  lBlock.style.webkitTransform = "rotateY(40deg) scale(0.8)";
+  lBlock.style.left = "300px";
+  lBlock.style.opacity = 0.9;
+  lBlock.style.zIndex = 2;
+  var centerBlock = getBlockByNum(num);
+  centerBlock.style.webkitTransform = "rotateY(0deg) scale(1)";
+  centerBlock.style.left = "400px";
+  centerBlock.style.opacity = 1;
+  centerBlock.style.zIndex = 3;
+  var rBlock = getBlockByNum(num + 1);
+  rBlock.style.webkitTransform = "rotateY(-40deg) scale(0.8)";
+  rBlock.style.left = "500px";
+  rBlock.style.opacity = 0.9;
+  rBlock.style.zIndex = 2;
+  var rrBlock = getBlockByNum(num + 2);
+  rrBlock.style.webkitTransform = "rotateY(-80deg) scale(0.5)";
+  rrBlock.style.left = "600px";
+  rrBlock.style.opacity = 0.3;
+  rrBlock.style.zIndex = 1;
+}
+function getBlockByNum(num){
+ if (num >= 0) {
+    return document.getElementById("object" + num);
+ } else {
+    var flowcount = document.getElementsByClassName("coverflow").length;
+    var newNum = num + flowcount;
+    document.getElementById("label").innerHTML = newNum;
+    return document.getElementById("object" + newNum);
+ }
 }
