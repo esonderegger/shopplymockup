7b04cf71120bf70fb874ed9462880d5c696786e0
Evan Sonderegger <evan.sonderegger@gmail.com>
2012-01-26 19:06:09 -0500

    starting to add deals

--- src/browse.py
+++ src/browse.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
-print '<h1>Browsing will go here.</h1>'
+print '<div id="container">'
+
+sampleFile = open('sampleDeals.txt', 'r')
+objectCount = 0
+
+for line in sampleFile:
+  lineList = line.split(';')
+  print '<div class="coverflow" id="object' + str(objectCount) + '">'
+  print '<h2>' + lineList[0] + ' - ' + lineList[1] + ' - ' + lineList[2] + '</h2>'
+  print '</div>'
+  objectCount += 1
+
+print '</div>'
