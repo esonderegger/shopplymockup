305d1a85ce3e70a28c35334154ad85c8a38955c5
Evan Sonderegger <evan.sonderegger@gmail.com>
2012-01-26 19:27:24 -0500

    tweaked css

--- src/js/shopply.js
+++ src/js/shopply.js
@@ -20,27 +20,27 @@ function initMainPage() {
 function moveCenter(num){
   var llBlock = getBlockByNum(num - 2);
   llBlock.style.webkitTransform = "rotateY(80deg) scale(0.5)";
-  llBlock.style.left = "200px";
+  llBlock.style.left = "0px";
   llBlock.style.opacity = 0.3;
   llBlock.style.zIndex = 1;
   var lBlock = getBlockByNum(num - 1);
   lBlock.style.webkitTransform = "rotateY(40deg) scale(0.8)";
-  lBlock.style.left = "300px";
+  lBlock.style.left = "150px";
   lBlock.style.opacity = 0.9;
   lBlock.style.zIndex = 2;
   var centerBlock = getBlockByNum(num);
   centerBlock.style.webkitTransform = "rotateY(0deg) scale(1)";
-  centerBlock.style.left = "400px";
+  centerBlock.style.left = "300px";
   centerBlock.style.opacity = 1;
   centerBlock.style.zIndex = 3;
   var rBlock = getBlockByNum(num + 1);
   rBlock.style.webkitTransform = "rotateY(-40deg) scale(0.8)";
-  rBlock.style.left = "500px";
+  rBlock.style.left = "475px";
   rBlock.style.opacity = 0.9;
   rBlock.style.zIndex = 2;
   var rrBlock = getBlockByNum(num + 2);
   rrBlock.style.webkitTransform = "rotateY(-80deg) scale(0.5)";
-  rrBlock.style.left = "600px";
+  rrBlock.style.left = "650px";
   rrBlock.style.opacity = 0.3;
   rrBlock.style.zIndex = 1;
 }
